6ec58e3be2dccc4e601ba0bc93fc103b6da6c0f1
Lara Kuhlman <you@example.com>
2022-05-03 10:25:23 -0400

    End of 11

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -6,17 +6,18 @@ import products from '../products'
 
 const ProductScreen = ({ match }) => {
     const product = products.find(p => p._id === match.params.id)
+    // const { id } = useParams()
     
-
-
   return <>
     <Link className='btn btn-light my-3' to='/'>
       Go Back
     </Link>
+
     <Row>
       <Col md={6}>
         <Image src={product.image} alt={product.name} fluid />
       </Col>
+
       <Col md={3}>
         <ListGroup variant='flush'>
           <ListGroup.Item>
@@ -26,14 +27,17 @@ const ProductScreen = ({ match }) => {
           <ListGroup.Item>
             <Rating value={product.rating} text={`${product.numReviews} reviews`} />
           </ListGroup.Item>
+
           <ListGroup.Item>
             Price: ${product.price}
           </ListGroup.Item>
+
           <ListGroup.Item>
             Description: {product.description}
           </ListGroup.Item>
         </ListGroup>
       </Col>
+
       <Col md={3}>
         <Card>
           <ListGroup variant='flush'>
