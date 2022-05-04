35524bb8205bf8367a41d2ee34459f929e1766b1
Lara Kuhlman <you@example.com>
2022-05-04 14:13:07 -0400

    Start of Lesson 30

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,19 +1,22 @@
-// import { combineReducers, applyMiddleware } from 'redux' OUTDATED
+import { combineReducers, applyMiddleware } from 'redux' 
 import { configureStore } from '@reduxjs/toolkit'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
+import { productListReducer } from './reducers/productReducers'
 
-// const reducer = combineReducers({}) OUTDATED
+const reducer = combineReducers({
+    productList: productListReducer
+})
 
 const initialState = {}
 
 const middleware = [thunk]
 
 const store = configureStore ({
-    reducer:{}, 
-        initialState
+    reducer, 
+    initialState
 })
-// , composeWithDevTools(applyMiddleware(...middleware))) OUTDATED
+composeWithDevTools(applyMiddleware(...middleware))
 
 
 export default store
