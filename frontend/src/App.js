418b1c43fba41432837735dbff7ec35d9ebde687
Lara Kuhlman <you@example.com>
2022-05-09 10:00:50 -0400

    Update User Profile

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,8 @@ import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
 import CartScreen from './screens/CartScreen'
 import LoginScreen from './screens/LoginScreen'
+import RegisterScreen from './screens/RegisterScreen'
+import ProfileScreen from './screens/ProfileScreen'
 
 const App = () => {
   return (
@@ -16,12 +18,12 @@ const App = () => {
           <Container>
             <Routes>
               <Route path='/login/' element={<LoginScreen />} /> 
+              <Route path='/register/' element={<RegisterScreen />} /> 
+              <Route path='/profile/' element={<ProfileScreen />} /> 
               <Route path='/product/:id' element={<ProductScreen />} />
-              {/* <Route path='/cart'> */}
-                <Route path='/cart/:id' element={<CartScreen />} />
-                <Route path='/cart/' element={<CartScreen />} />
-                <Route path='/' element={<HomeScreen />} />  
-              {/* </Route>   */}
+              <Route path='/cart/:id' element={<CartScreen />} />
+              <Route path='/cart/' element={<CartScreen />} />
+              <Route path='/' element={<HomeScreen />} />  
             </Routes>
           </Container>
         </main>
