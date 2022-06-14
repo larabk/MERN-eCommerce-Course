b83024f7cce415f991d87cc7b797fa568964034a
Lara Bartol-Kuhlman <larakuhlman@gmail.com>
2022-06-14 16:14:40 -0400

    Create and Update Products Endpoints

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,7 +2,11 @@ import { combineReducers, applyMiddleware } from 'redux'
 import { configureStore } from '@reduxjs/toolkit'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
-import { productListReducer, productDetailsReducer } from './reducers/productReducers'
+import { 
+    productListReducer, 
+    productDetailsReducer, 
+    productDeleteReducer 
+} from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
 import { 
     userLoginReducer, 
@@ -22,6 +26,7 @@ import {
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
+    productDelete:productDeleteReducer,
     cart: cartReducer,
     userLogin: userLoginReducer,
     userRegister: userRegisterReducer,
