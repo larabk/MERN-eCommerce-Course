bf9022fedf133df137e0db1f91945dcde2c18724
Lara Bartol-Kuhlman <larakuhlman@gmail.com>
2022-06-14 15:56:19 -0400

    Admin Delete Products

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -49,11 +49,11 @@ const Header = () => {
                 <NavDropdown.Item>Users</NavDropdown.Item>
               </LinkContainer>
 
-              <LinkContainer to='/admin/productlsit'>
+              <LinkContainer to='/admin/productlist'>
                 <NavDropdown.Item>Products</NavDropdown.Item>
               </LinkContainer>
 
-              <LinkContainer to='/admin/orderlsit'>
+              <LinkContainer to='/admin/orderlist'>
                 <NavDropdown.Item>Orders</NavDropdown.Item>
               </LinkContainer>
               
--- frontend/src/screens/UserEditScreen.js
+++ frontend/src/screens/UserEditScreen.js
@@ -9,106 +9,104 @@ import { getUserDetails, updateUser } from '../actions/userActions'
 import { USER_UPDATE_RESET } from '../constants/userConstants'
 
 const UserEditScreen = () => {
-    
-    const {id} = useParams()
+  const { id } = useParams()
 
-    const location = useLocation()
-    const dispatch = useDispatch()
-    const navigate = useNavigate()
+  const location = useLocation()
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
 
-    const [name, setName] = useState('')
-    const [email, setEmail] = useState('')
-    const [isAdmin, setIsAdmin] = useState(false)
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [isAdmin, setIsAdmin] = useState(false)
 
-    const redirect = new URLSearchParams(location.search).get('redirect') ?
-    new URLSearchParams(location.search).get('redirect') : '/'
+  const redirect = new URLSearchParams(location.search).get('redirect')
+    ? new URLSearchParams(location.search).get('redirect')
+    : '/'
 
-    const userDetails = useSelector( (state) => state.userDetails)
-    const { loading, error, user } = userDetails
+  const userDetails = useSelector((state) => state.userDetails)
+  const { loading, error, user } = userDetails
 
-    const userUpdate = useSelector( (state) => state.userUpdate)
-    const { loading: loadingUpdate, error: errorUpdate, success: successUpdate } = userUpdate
+  const userUpdate = useSelector((state) => state.userUpdate)
+  const {
+    loading: loadingUpdate,
+    error: errorUpdate,
+    success: successUpdate,
+  } = userUpdate
 
-    // const redirectAdmin = new URLSearchParams(location.search).get('redirectAdmin') ?
-    // new URLSearchParams(location.search).get('redirectAdmin') : '/admin/userList'
+  // const redirectAdmin = new URLSearchParams(location.search).get('redirectAdmin') ?
+  // new URLSearchParams(location.search).get('redirectAdmin') : '/admin/userList'
 
-    useEffect(() => {
-        if(successUpdate) {
-            dispatch({ type: USER_UPDATE_RESET })
-            navigate(redirect)
-        } else {
-            if(!user.name || user._id !== id) {
-                dispatch(getUserDetails(id))
-            } else {
-                setName(user.name)
-                setEmail(user.email)
-                setIsAdmin(user.isAdmin)
-            }
-        }
-
-    }, [dispatch, redirect, navigate, id, user, successUpdate])
-
-    const submitHandler = (e) => {
-        e.preventDefault()
-        dispatch(updateUser({ _id: userDetails, name, email, isAdmin }))
-        
+  useEffect(() => {
+    if (successUpdate) {
+      dispatch({ type: USER_UPDATE_RESET })
+      navigate(redirect)
+    } else {
+      if (!user.name || user._id !== id) {
+        dispatch(getUserDetails(id))
+      } else {
+        setName(user.name)
+        setEmail(user.email)
+        setIsAdmin(user.isAdmin)
+      }
     }
-  
-    return (
-        <>
-        <Link to='/admin/userList' className='btn btn-light my-3'>
-            Go Back
-            </Link>
-        <FormContainer>
+  }, [dispatch, redirect, navigate, id, user, successUpdate])
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    dispatch(updateUser({ _id: userDetails, name, email, isAdmin }))
+  }
+
+  return (
+    <>
+      <Link to="/admin/userList" className="btn btn-light my-3">
+        Go Back
+      </Link>
+      <FormContainer>
         <h1>Edit User</h1>
         {loadingUpdate && <Loader />}
-        {errorUpdate && <Message variant='danger'>{errorUpdate}</Message>}
-        {loading ? <Loader /> : error ? <Message variant='danger'>
-            {error}</Message> : (
-                 <Form onSubmit={submitHandler}>
-            
-                 <Form.Group controlId='name'>
-                     <Form.Label>Name</Form.Label>
-                     <Form.Control   
-                         type='name' 
-                         placeholder='Enter Name'
-                         value={name} 
-                         onChange={(e) => setName(e.target.value)}>
-                     </Form.Control>
-                 </Form.Group>
-     
-                 <Form.Group controlId='email'>
-                     <Form.Label>Email Address</Form.Label>
-                     <Form.Control   
-                         type='email' 
-                         placeholder='Enter Email'
-                         value={email} 
-                         onChange={(e) => setEmail(e.target.value)}>
-                     </Form.Control>
-                 </Form.Group>
-     
-                 <Form.Group controlId='isAdmin'>
-                     
-                     <Form.Check   
-                        type='checkbox' 
-                        label='Is Admin'
-                        checked={isAdmin}
-                        onChange={(e) => setIsAdmin(e.target.checked)}>
-                     </Form.Check>
-                 </Form.Group>
-     
-                
-     
-                 <Button type='submit' variant='primary'>
-                     Update
-                 </Button>
-             </Form>          
+        {errorUpdate && <Message variant="danger">{errorUpdate}</Message>}
+        {loading ? (
+          <Loader />
+        ) : error ? (
+          <Message variant="danger">{error}</Message>
+        ) : (
+          <Form onSubmit={submitHandler}>
+            <Form.Group controlId="name">
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                type="name"
+                placeholder="Enter Name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
 
-            )}
-       
-    </FormContainer>
-            </>
+            <Form.Group controlId="email">
+              <Form.Label>Email Address</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
 
+            <Form.Group controlId="isAdmin">
+              <Form.Check
+                type="checkbox"
+                label="Is Admin"
+                checked={isAdmin}
+                onChange={(e) => setIsAdmin(e.target.checked)}
+              ></Form.Check>
+            </Form.Group>
+
+            <Button type="submit" variant="primary">
+              Update
+            </Button>
+          </Form>
+        )}
+      </FormContainer>
+    </>
   )
 }
 
