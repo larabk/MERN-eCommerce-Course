91a95a75780b67e42c4f949281771deedacf09f9
Lara Bartol-Kuhlman <larakuhlman@gmail.com>
2022-06-15 14:53:50 -0400

    Edit Product Screen

--- frontend/src/screens/ProductListScreen.js
+++ frontend/src/screens/ProductListScreen.js
@@ -5,7 +5,8 @@ import { Table, Button, Row, Col } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
-import { listProducts, deleteProduct } from '../actions/productActions'
+import { listProducts, deleteProduct, createProduct } from '../actions/productActions'
+import { PRODUCT_CREATE_RESET } from '../constants/productConstants'
 
 const ProductListScreen = () => {
     const dispatch = useDispatch()
@@ -18,6 +19,14 @@ const ProductListScreen = () => {
     const productDelete = useSelector(state => state.productDelete)
     const { loading:loadingDelete, error:errorDelete, success:successDelete } = productDelete
 
+    const productCreate = useSelector(state => state.productCreate)
+    const { 
+        loading:loadingCreate, 
+        error:errorCreate, 
+        success:successCreate, 
+        product:createdProduct 
+    } = productCreate
+
     const userLogin = useSelector(state => state.userLogin)
     const { userInfo } = userLogin
 
@@ -25,22 +34,26 @@ const ProductListScreen = () => {
     new URLSearchParams(location.search).get('redirect') : '/login'
 
     useEffect(() => {
-        if(userInfo && userInfo.isAdmin) {
-            dispatch(listProducts())
-        } else {
+        dispatch({ type: PRODUCT_CREATE_RESET })
+
+        if(!userInfo.isAdmin) {
             navigate(redirect)
         }
-        // dispatch(listUsers())
-    }, [dispatch, navigate, userInfo, successDelete])
-
+        if(successCreate) {
+            navigate(`/admin/product/${createProduct._id}/edit`)
+        } else {
+            dispatch(listProducts())
+        }
+    }, [dispatch, navigate, userInfo, successDelete, successCreate, createdProduct])
+    
     const deleteHandler = (id) => {
         if (window.confirm('Are you sure?')) {
             dispatch(deleteProduct(id))
         }
     }
     
-    const createProductHandler = (product) => {
-        //create product
+    const createProductHandler = () => {
+        dispatch(createProduct())
     }
     return (
     <>
@@ -57,6 +70,10 @@ const ProductListScreen = () => {
 
         {loadingDelete && <Loader />}
         {errorDelete && <Message variant='danger'>{errorDelete}</Message>}
+
+        {loadingCreate && <Loader />}
+        {errorCreate && <Message variant='danger'>{errorCreate}</Message>}
+
         {loading ? (
             <Loader /> 
         ) : error ? (
