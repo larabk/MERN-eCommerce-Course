d346f2141275052f6addc709c8afd5ab710f1b90
Lara Bartol-Kuhlman <larakuhlman@gmail.com>
2022-06-30 15:44:23 -0400

    Ready for bug fixes and Heroku

--- backend/server.js
+++ backend/server.js
@@ -34,9 +34,21 @@ app.use('/api/upload', uploadRoutes)
 app.get('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 )
-
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend',
+  'build', 'index.html')))
+
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
+}
+
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/frontend/build')))
@@ -44,11 +56,7 @@ if (process.env.NODE_ENV === 'production') {
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
   )
-} else {
-  app.get('/', (req, res) => {
-    res.send('API is running....')
-  })
-}
+} 
 
 app.use(notFound)
 app.use(errorHandler)
